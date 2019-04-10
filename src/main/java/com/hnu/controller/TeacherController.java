de0d6316f0f30618f671bad797bdf1370a3ea216
zpt <2470826402@qq.com>
2019-04-10 11:45:28 +0800

    学生端修改完成

--- src/main/java/com/hnu/controller/TeacherController.java
+++ src/main/java/com/hnu/controller/TeacherController.java
@@ -18,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
